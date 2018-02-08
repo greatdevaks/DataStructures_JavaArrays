03221d3bb1e4415d0babae0c7b9c921697a3f0ba
greatdevaks <greatdevaks@gmail.com>
2018-02-09 03:01:32 +0530

    Printing an Array

--- src/arrays/greatdevaks/Arrays.java
+++ src/arrays/greatdevaks/Arrays.java
@@ -5,6 +5,13 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//declaring an array using literals
+		int my_arr[] = new int[] {1, 2, 3, 4, 5};
+		System.out.println("Printing an array:");
+		for(int i = 0; i < my_arr.length; i++) {
+			System.out.println(my_arr[i]);
+		}
+		
 		//declaring and initializing an array
 		int arr[] = new int[5];
 		for(int i = 0; i < arr.length; i++) {
@@ -13,7 +20,7 @@ public class Arrays {
 		
 		//traversing linear array and printing
 		TraversingLinearArray_P1 objTLA = new TraversingLinearArray_P1();
-		System.out.println("Traversing Linear Array:");
+		System.out.println("\nTraversing Linear Array:");
 		objTLA.TraversingLinearArray(arr);
 		
 		//traversing linear array, performing operations, and getting modified array
